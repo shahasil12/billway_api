35dc4db09622517ca71b216be50211d71f7ee42d
shahasil12 <shahasil54@gmail.com>
2026-08-06 12:42:12 +0530

    Fix 500 error on reports endpoint by adding missing imports

--- billing/views.py
+++ billing/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.utils import timezone
 from django.db.models import Sum
+from django.db.models.functions import TruncDate
+from datetime import datetime, timedelta
 from .models import Invoice, Customer, Product
 from .serializers import InvoiceSerializer
 
